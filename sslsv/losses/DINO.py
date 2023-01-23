aba25e933597386bcced28d779ea3f2204d73082
Theo Lepage <theo.lepage@outlook.com>
2023-01-23 14:02:32 +0100

    fix(dino): make DINO converge

--- sslsv/losses/DINO.py
+++ sslsv/losses/DINO.py
@@ -12,16 +12,11 @@ class DINOLoss(nn.Module):
         teacher_temp,
         teacher_temp_warmup,
         teacher_temp_warmup_epochs,
-        center_momentum=0.9,
-        nb_local_views=4,
-        nb_global_views=2
+        center_momentum=0.9
     ):
         super().__init__()
 
         self.epoch = 0
-
-        self.nb_local_views = nb_local_views
-        self.nb_global_views = nb_global_views
 
         self.student_temp = student_temp
         self.teacher_temp_warmup = torch.linspace(
@@ -32,7 +27,7 @@ class DINOLoss(nn.Module):
         self.teacher_temp = teacher_temp
 
         self.center_momentum = center_momentum
-        self.register_buffer('center', torch.zeros(1, nb_prototypes))
+        self.register_buffer('center', torch.zeros(1, 1, nb_prototypes))
 
     def _get_teacher_temp(self):
         if self.epoch < len(self.teacher_temp_warmup):
@@ -40,31 +35,29 @@ class DINOLoss(nn.Module):
         return self.teacher_temp
 
     def forward(self, S, T):
-        # Student
-        student_out = S / self.student_temp
-        student_out = student_out.chunk(self.nb_local_views)
+        S = S / self.student_temp
 
-        # Teacher
-        teacher_out = (T - self.center) / self._get_teacher_temp()
-        teacher_out = F.softmax(teacher_out, dim=-1)
-        teacher_out = teacher_out.detach().chunk(self.nb_global_views)
+        T_ = F.softmax((T - self.center) / self._get_teacher_temp(), dim=-1)
+        T_ = T_.detach()
 
         loss = 0
         nb_loss_terms = 0
+        for i in range(S.size(1)):
+            for j in range(T.size(1)):
+                if i == j: continue
 
-        for iq, q in enumerate(teacher_out):
-            for iv, v in enumerate(student_out):
-                if iv == iq: continue
-                loss += torch.sum(-q * F.log_softmax(v, dim=-1), dim=-1).mean()
+                loss += torch.sum(-T_[:, j] * F.log_softmax(S[:, i], dim=-1), dim=-1).mean()
                 nb_loss_terms += 1
 
         loss /= nb_loss_terms
+
         self.update_center(T)
+
         return loss
 
     @torch.no_grad()
     def update_center(self, T):
-        new_center = torch.mean(T, dim=0, keepdim=True)
+        new_center = torch.mean(T, dim=(0, 1), keepdim=True)
         self.center = (
             self.center * self.center_momentum +
             new_center * (1 - self.center_momentum)
--- sslsv/models/DINO.py
+++ sslsv/models/DINO.py
@@ -19,25 +19,16 @@ class DINOHead(nn.Module):
 
         self.mlp = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
-            nn.GELU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.GELU(),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
             nn.Linear(hidden_dim, bottleneck_dim)
         )
-
-        self.apply(self._init_weights)
 
         self.last_layer = nn.utils.weight_norm(
             nn.Linear(bottleneck_dim, output_dim, bias=False)
         )
         self.last_layer.weight_g.data.fill_(1)
         self.last_layer.weight_g.requires_grad = False
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            nn.init.trunc_normal_(m.weight, std=0.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         x = self.mlp(x)
@@ -49,21 +40,18 @@ class DINOHead(nn.Module):
 @dataclass
 class DINOConfig(BaseMomentumModelConfig):
 
-    head_hidden_dim: int = 2048
-    head_bottleneck_dim: int = 256
-    head_output_dim: int = 65536
+    start_tau: float = 0.996
 
-    clip_grad: bool = False
-    clip_grad_max: float = 3.0
+    head_hidden_dim: int = 2048
+    head_bottleneck_dim: int = 128
+    head_output_dim: int = 16384
 
     freeze_last_layer: int = 1
 
     student_temperature: float = 0.1
-    teacher_temperature: float = 0.07
+    teacher_temperature: float = 0.04
     teacher_temperature_warmup: float = 0.04
-    teacher_temperature_warmup_epochs: float = 30
-
-    small_frame_length: int = 32000
+    teacher_temperature_warmup_epochs: float = 0
 
 
 class DINO(BaseMomentumModel):
@@ -73,10 +61,7 @@ class DINO(BaseMomentumModel):
 
         self.current_epoch = 0
 
-        self.clip_grad = config.clip_grad
-        self.clip_grad_max = config.clip_grad_max
         self.freeze_last_layer = config.freeze_last_layer
-        self.small_frame_length = config.small_frame_length
 
         self.head = DINOHead(
             input_dim=self.encoder.encoder_dim,
@@ -109,23 +94,25 @@ class DINO(BaseMomentumModel):
         X_2_large = X[:, 1, :]
 
         # Extract local views
-        X_1_small_A = X_1_large[:, :self.small_frame_length]
-        X_1_small_B = X_1_large[:, self.small_frame_length:]
-        X_2_small_A = X_2_large[:, :self.small_frame_length]
-        X_2_small_B = X_2_large[:, self.small_frame_length:]
+        #small_frame_length = X.size(-1) // 2
+        #X_1_small_A = X_1_large[:, :small_frame_length]
+        #X_1_small_B = X_1_large[:, small_frame_length:]
+        #X_2_small_A = X_2_large[:, :small_frame_length]
+        #X_2_small_B = X_2_large[:, small_frame_length:]
 
-        # Compute embeddings
-        S = torch.cat((
-            self.head(self.encoder(X_1_small_A)),
-            self.head(self.encoder(X_1_small_B)),
-            self.head(self.encoder(X_2_small_A)),
-            self.head(self.encoder(X_2_small_B))
-        ))
+        S = torch.stack((
+            self.head(self.encoder(X_1_large)),
+            self.head(self.encoder(X_2_large)),
+            #self.head(self.encoder(X_1_small_A)),
+            #self.head(self.encoder(X_1_small_B)),
+            #self.head(self.encoder(X_2_small_A)),
+            #self.head(self.encoder(X_2_small_B))
+        ), dim=1)
 
-        T = torch.cat((
+        T = torch.stack((
             self.head_momentum(self.encoder_momentum(X_1_large)),
             self.head_momentum(self.encoder_momentum(X_2_large))
-        ))
+        ), dim=1)
 
         return S, T
 
@@ -158,14 +145,6 @@ class DINO(BaseMomentumModel):
         self.loss_fn.epoch = epoch
 
     def on_after_backward(self):
-        # Perform gradient clipping on encoder
-        if self.clip_grad:
-            for p in self.encoder.parameters():
-                if p.grad is not None:
-                    clip = self.clip_grad_max / (p.grad.data.norm(2) + 1e-6)
-                    if clip < 1:
-                        p.grad.data.mul_(clip)
-
         # Freeze last layer of head
         if self.current_epoch < self.freeze_last_layer:
             for p in self.head.last_layer.parameters():
