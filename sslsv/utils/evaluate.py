cd48714b297ec26d963b2fd3b4b75d29b51f76ad
Theo Lepage <theo.lepage@outlook.com>
2023-02-15 14:12:48 +0100

    refactor(evaluate): simplify code to compute minDCF

--- sslsv/utils/evaluate.py
+++ sslsv/utils/evaluate.py
@@ -110,66 +110,44 @@ def score_trials(trials_path, embeddings):
     return scores, labels
 
 
-def compute_eer(scores, labels):
-    fpr, tpr, thresholds = roc_curve(labels, scores, pos_label=1)
-    fnr = 1 - tpr
-    idxE = np.nanargmin(np.abs(fnr - fpr))
-    eer  = max(fpr[idxE], fnr[idxE]) * 100
-    return eer
+def compute_eer(fprs, fnrs):
+    idx = np.nanargmin(np.abs(fnrs - fprs))
+    eer  = max(fprs[idx], fnrs[idx]) * 100
+    return eer, idx
 
 
-def compute_error_rates(scores, labels):
-      # Sort scores from smallest to largest.
-      # Scores are the thresholds at which the the error-rates are evaluated.
-      sorted_indexes, thresholds = zip(*sorted(
-          [(index, threshold) for index, threshold in enumerate(scores)],
-          key=itemgetter(1)))
-      labels = [labels[i] for i in sorted_indexes]
-
-      # Determine false negative rates and false positive rates for each threshold.
-      fnrs = []
-      fprs = []
-      for i in range(0, len(labels)):
-          if i == 0:
-              fnrs.append(labels[i])
-              fprs.append(1 - labels[i])
-          else:
-              fnrs.append(fnrs[i -1] + labels[i])
-              fprs.append(fprs[i -1] + 1 - labels[i])
-
-      fnrs_norm = sum(labels)
-      fnrs = [x / float(fnrs_norm) for x in fnrs]
-
-      fprs_norm = len(labels) - fnrs_norm
-      fprs = [1 - x / float(fprs_norm) for x in fprs]
-
-      return fnrs, fprs
-
-
-def compute_min_dcf(fnrs, fprs, p_target=0.01, c_miss=1, c_fa=1):
+def compute_min_dcf(fprs, fnrs, p_target=0.01, c_miss=1, c_fa=1):
     # Equations are from Section 3 of
     # NIST 2016 Speaker Recognition Evaluation Plan
 
     # Equation (2)
     min_c_det = float('inf')
+    min_c_det_idx = None
     for i in range(0, len(fnrs)):
         c_det = c_miss * fnrs[i] * p_target + c_fa * fprs[i] * (1 - p_target)
         if c_det < min_c_det:
             min_c_det = c_det
+            min_c_det_idx = i
 
     # Equations (3) and (4)
     c_def = min(c_miss * p_target, c_fa * (1 - p_target))
     min_dcf = min_c_det / c_def
 
-    return min_dcf
+    return min_dcf, min_c_det_idx
 
 
 def evaluate(embeddings, trials):
     scores, labels = score_trials(trials, embeddings)
 
-    eer = compute_eer(scores, labels)
+    fprs, tprs, thresholds = roc_curve(
+        labels,
+        scores,
+        pos_label=1,
+        drop_intermediate=False
+    )
+    fnrs = 1 - tprs
 
-    fnrs, fprs = compute_error_rates(scores, labels)
-    min_dcf_001 = compute_min_dcf(fnrs, fprs, p_target=0.01)
+    eer, _ = compute_eer(fprs, fnrs)
+    min_dcf, _ = compute_min_dcf(fprs, fnrs, p_target=0.01)
 
-    return { 'test_eer': eer, 'test_min_dcf_001': min_dcf_001 }
+    return { 'test_eer': eer, 'test_min_dcf_001': min_dcf }
