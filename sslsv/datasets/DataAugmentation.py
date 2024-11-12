7e923e55f9e0dd7d58871d4c3b079934cad5c4da
Theo Lepage <theo.lepage@outlook.com>
2024-11-12 18:05:57 +0100

    fix(dino): add data aug strategy 'all'

--- sslsv/datasets/DataAugmentation.py
+++ sslsv/datasets/DataAugmentation.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Tuple
+from enum import Enum
 
 from pathlib import Path
 import glob
@@ -11,6 +12,23 @@ from scipy.signal import convolve
 from sslsv.datasets.utils import load_audio, read_audio
 
 
+class DataAugmentationStrategyEnum(Enum):
+    """
+    Enumeration representing data-augmentation strategies.
+
+    Attributes:
+        REVERB (str): Apply reverberation.
+        NOISE (str): Apply noise.
+        BOTH (str): Apply reverberation and noise.
+        ALL (str): Apply reverberation or noise or both or nothing.
+    """
+
+    REVERB = "reverb"
+    NOISE = "noise"
+    BOTH = "reverb+noise"
+    ALL = "all"
+
+
 @dataclass
 class DataAugmentationConfig:
     """
@@ -18,9 +36,7 @@ class DataAugmentationConfig:
 
     Attributes:
         enable (bool): Whether data-augmentation is enabled.
-        rir (bool): Whether RIR (reverb) augmentation is enabled.
-        musan (bool): Whether MUSAN (noise) augmentation is enabled.
-        musan_nb_iters (int): Number of iterations for MUSAN augmentation.
+        strategy (DataAugmentationStrategyEnum): Data-augmentation techniques to apply.
         musan_noise_snr (Tuple[int, int]): Signal-to-noise ratio (SNR) range for MUSAN noise.
         musan_speech_snr (Tuple[int, int]): Signal-to-noise ratio (SNR) range for MUSAN speech.
         musan_music_snr (Tuple[int, int]): Signal-to-noise ratio (SNR) range for MUSAN music.
@@ -31,9 +47,7 @@ class DataAugmentationConfig:
 
     enable: bool = True
 
-    rir: bool = True
-    musan: bool = True
-    musan_nb_iters: int = 1
+    strategy: DataAugmentationStrategyEnum = DataAugmentationStrategyEnum.BOTH
 
     musan_noise_snr: Tuple[int, int] = (0, 15)
     musan_speech_snr: Tuple[int, int] = (13, 20)
@@ -133,17 +147,18 @@ class DataAugmentation:
         min_, max_ = CATEGORY_TO_NUM[category]
         return random.randint(min_, max_)
 
-    def add_noise(self, audio: np.ndarray, category: str) -> np.ndarray:
+    def add_noise(self, audio: np.ndarray) -> np.ndarray:
         """
-        Add noise to an audio signal using a randomly selected noise from a MUSAN category.
+        Add noise to an audio signal using a randomly selected noise from MUSAN.
 
         Args:
             audio (numpy.ndarray): Input audio.
-            category (str): MUSAN category.
 
         Returns:
             numpy.ndarray: Output audio.
         """
+        category = random.choice(["speech", "noise", "music"])
+
         noise_files = random.sample(
             self.musan_files[category], self._get_noise_num(category)
         )
@@ -173,15 +188,23 @@ class DataAugmentation:
         Returns:
             np.ndarray: Output audio.
         """
-        if self.config.rir:
+        if self.config.strategy == DataAugmentationStrategyEnum.REVERB:
             audio = self.reverberate(audio)
-        if self.config.musan:
-            for _ in range(self.config.musan_nb_iters):
-                musan_category = random.randint(0, 2)
-                if musan_category == 0:
-                    audio = self.add_noise(audio, "music")
-                elif musan_category == 1:
-                    audio = self.add_noise(audio, "speech")
-                elif musan_category == 2:
-                    audio = self.add_noise(audio, "noise")
+        elif self.config.strategy == DataAugmentationStrategyEnum.NOISE:
+            audio = self.add_noise(audio)
+        elif self.config.strategy == DataAugmentationStrategyEnum.BOTH:
+            audio = self.reverberate(audio)
+            audio = self.add_noise(audio)
+        elif self.config.strategy == DataAugmentationStrategyEnum.ALL:
+            aug_type = random.randint(0, 3)
+            if aug_type == 0:
+                pass
+            elif aug_type == 1:
+                audio = self.reverberate(audio)
+            elif aug_type == 2:
+                audio = self.add_noise(audio)
+            elif aug_type == 3:
+                audio = self.reverberate(audio)
+                audio = self.add_noise(audio)
+
         return audio
