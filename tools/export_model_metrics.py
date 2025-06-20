f362b2ae5a780a8605df7379e4f25a2664b7f43b
Theo Lepage <theo@theolepage.com>
2025-06-20 13:48:28 +0200

    misc(configs): eval downstream classification tasks on full-length utterances

--- tools/export_model_metrics.py
+++ tools/export_model_metrics.py
@@ -22,6 +22,31 @@ def export_model_metrics(args: argparse.Namespace):
 
     res = {}
 
+    # train_files = list(Path(args.configs).rglob("training.json"))
+    # for train_path in train_files:
+    #     if "label-efficient" not in str(train_path):
+    #         continue
+
+    #     with open(train_path, "r") as file:
+    #         train = json.load(file)
+
+    #     model_name = str(train_path)[7:-14]
+
+    #     if "99" not in train:
+    #         continue
+
+    #     res[model_name] = {
+    #         "VoxCeleb1-O___EER (%)": formatf2(
+    #             train["99"]["val/sv_cosine/voxceleb1_test_O/eer"]
+    #         ),
+    #         "VoxCeleb1-O___minDCF": formatf4(
+    #             train["99"]["val/sv_cosine/voxceleb1_test_O/mindcf"]
+    #         ),
+    #     }
+
+    # print(json.dumps(res, indent=4))
+    # return
+
     configs = list(Path(args.configs).rglob("config.yml"))
 
     for config_path in configs:
@@ -40,17 +65,17 @@ def export_model_metrics(args: argparse.Namespace):
 
         model_name = str(config_path)[7:-11]
 
-        if "export" in model_name:
-            model_name = model_name[14:]
+        res[model_name] = {}
 
-        res[model_name] = {
-            "VoxCeleb1-O___EER (%)": formatf2(
-                eval["test/sv_cosine/voxceleb1_test_O/eer"]
-            ),
-            "VoxCeleb1-O___minDCF": formatf4(
-                eval["test/sv_cosine/voxceleb1_test_O/mindcf"]
-            ),
-        }
+        if "test/sv_cosine/voxceleb1_test_O/eer" in eval:
+            res[model_name] = {
+                "VoxCeleb1-O___EER (%)": formatf2(
+                    eval["test/sv_cosine/voxceleb1_test_O/eer"]
+                ),
+                "VoxCeleb1-O___minDCF": formatf4(
+                    eval["test/sv_cosine/voxceleb1_test_O/mindcf"]
+                ),
+            }
 
         if "test/sv_cosine/voxceleb1_test_E/eer" in eval:
             res[model_name].update(
@@ -96,6 +121,62 @@ def export_model_metrics(args: argparse.Namespace):
                     ),
                     "VOiCES___minDCF": formatf4(
                         eval["test/sv_cosine/voices2019_dev/mindcf"]
+                    ),
+                }
+            )
+
+        if "test/classification/voxlingua107_train/accuracy" in eval:
+            res[model_name].update(
+                {
+                    "VoxLingua107___Accuracy": round(
+                        eval["test/classification/voxlingua107_train/accuracy"] * 100, 2
+                    ),
+                    "VoxLingua107___F1 score": round(
+                        eval["test/classification/voxlingua107_train/f1_score"] * 100, 2
+                    ),
+                }
+            )
+
+        if "test/classification/cremad_train/accuracy" in eval:
+            res[model_name].update(
+                {
+                    "CREMA-D___Accuracy": round(
+                        eval["test/classification/cremad_train/accuracy"] * 100, 2
+                    ),
+                    "CREMA-D___F1 score": round(
+                        eval["test/classification/cremad_train/f1_score"] * 100, 2
+                    ),
+                }
+            )
+
+        if "test/classification/voxceleb1_train_speaker/accuracy" in eval:
+            res[model_name].update(
+                {
+                    "VoxCeleb1-Speaker___Accuracy": round(
+                        eval["test/classification/voxceleb1_train_speaker/accuracy"]
+                        * 100,
+                        2,
+                    ),
+                    "VoxCeleb1-Speaker___F1 score": round(
+                        eval["test/classification/voxceleb1_train_speaker/f1_score"]
+                        * 100,
+                        2,
+                    ),
+                }
+            )
+
+        if "test/classification/voxceleb1_train_gender/accuracy" in eval:
+            res[model_name].update(
+                {
+                    "VoxCeleb1-Gender___Accuracy": round(
+                        eval["test/classification/voxceleb1_train_gender/accuracy"]
+                        * 100,
+                        2,
+                    ),
+                    "VoxCeleb1-Gender___F1 score": round(
+                        eval["test/classification/voxceleb1_train_gender/f1_score"]
+                        * 100,
+                        2,
                     ),
                 }
             )
